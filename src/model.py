0e8fb3b165cf34611afaa1a07e729efa55fcfda8
unknown <khongtrunght@gmail.com>
2022-06-02 11:05:22 +0700

    add pretrain unet

--- src/model.py
+++ src/model.py
@@ -50,9 +50,10 @@ def get_net(num_classes, arch='normal'):
             num_classes, num_classes, kernel_size=64, padding=16, stride=32))
         W = bilinear_kernel(num_classes, num_classes, 64)
         net.transpose_conv.weight.data.copy_(W)
-
-    else:
+    elif arch == 'unet':
         net = Unet(3, num_classes)
+    else:
+        net = UnetPretrain(3, num_classes)
 
     return net
 
@@ -111,9 +112,88 @@ class Unet(nn.Module):
         return y
 
 
+def convrelu(in_channels, out_channels, kernel, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
+        nn.ReLU(inplace=True),
+    )
+
+
+class UnetPretrain(nn.Module):
+    def __init__(self, in_channels, n_class):
+        super(UnetPretrain, self).__init__()
+        self.base_model = torchvision.models.resnet18(pretrained=True)
+        self.base_layers = list(self.base_model.children())
+
+        # size=(N, 64, x.H/2, x.W/2)
+        self.layer0 = nn.Sequential(*self.base_layers[:3])
+        self.layer0_1x1 = convrelu(64, 64, 1, 0)
+        # size=(N, 64, x.H/4, x.W/4)
+        self.layer1 = nn.Sequential(*self.base_layers[3:5])
+        self.layer1_1x1 = convrelu(64, 64, 1, 0)
+        self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)
+        self.layer2_1x1 = convrelu(128, 128, 1, 0)
+        self.layer3 = self.base_layers[6]  # size=(N, 256, x.H/16, x.W/16)
+        self.layer3_1x1 = convrelu(256, 256, 1, 0)
+        self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
+        self.layer4_1x1 = convrelu(512, 512, 1, 0)
+
+        self.upsample = nn.Upsample(
+            scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv_up3 = convrelu(256 + 512, 512, 3, 1)
+        self.conv_up2 = convrelu(128 + 512, 256, 3, 1)
+        self.conv_up1 = convrelu(64 + 256, 256, 3, 1)
+        self.conv_up0 = convrelu(64 + 256, 128, 3, 1)
+
+        self.conv_original_size0 = convrelu(3, 64, 3, 1)
+        self.conv_original_size1 = convrelu(64, 64, 3, 1)
+        self.conv_original_size2 = convrelu(64 + 128, 64, 3, 1)
+
+        self.conv_last = nn.Conv2d(64, n_class, 1)
+
+    def forward(self, images):
+        x_original = self.conv_original_size0(images)
+        x_original = self.conv_original_size1(x_original)
+
+        layer0 = self.layer0(images)
+        layer1 = self.layer1(layer0)
+        layer2 = self.layer2(layer1)
+        layer3 = self.layer3(layer2)
+        layer4 = self.layer4(layer3)
+
+        layer4 = self.layer4_1x1(layer4)
+        x = self.upsample(layer4)
+        layer3 = self.layer3_1x1(layer3)
+        x = torch.cat([x, layer3], dim=1)
+        x = self.conv_up3(x)
+
+        x = self.upsample(x)
+        layer2 = self.layer2_1x1(layer2)
+        x = torch.cat([x, layer2], dim=1)
+        x = self.conv_up2(x)
+
+        x = self.upsample(x)
+        layer1 = self.layer1_1x1(layer1)
+        x = torch.cat([x, layer1], dim=1)
+        x = self.conv_up1(x)
+
+        x = self.upsample(x)
+        layer0 = self.layer0_1x1(layer0)
+        x = torch.cat([x, layer0], dim=1)
+        x = self.conv_up0(x)
+
+        x = self.upsample(x)
+        x = torch.cat([x, x_original], dim=1)
+        x = self.conv_original_size2(x)
+
+        out = self.conv_last(x)
+
+        return out
+
+
 if __name__ == '__main__':
-    image = torch.randn(1, 3, 256, 256)
-    pretrained_net = torchvision.models.resnet18(pretrained=True)
-    net = torch.nn.Sequential(*list(pretrained_net.children())[:-2])
-    out = net(image)
-    print(out.shape)
+    image = torch.randn(1, 3, 512, 512)
+
+    net = UnetPretrain(3, 21)
+    print(net(image).shape)
